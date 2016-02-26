6757ac62c711f679ecff18dad459fc0eba411ee0
Matti Jokitulppo <melonmanchan@gmail.com>
2016-02-26 11:32:57 +0200

    Cleaning up the main script

--- resumeos.py
+++ resumeos.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-__author__ = "Matti Jokitulppo"
+__author__ = "Matti Jokitulppo <melonmanchan@gmail.com>"
 
 from distutils.dir_util import copy_tree
-from jinja2 import Template
-from pyfiglet import Figlet
+from jinja2             import Template
+from pyfiglet           import Figlet
 
 import click
 import os
@@ -25,37 +25,40 @@ def render_template_file(file_name, context):
 @click.option('--name',
             default='my-own-cool-os',
             help='The name of your OS',
-            prompt='What is the name of your OS? ')
+            prompt='What is the name of your hobby OS? ')
 @click.option('--output',
             default='Hello, world!',
             help='The string output by your OS on boot',
-            prompt='What do you want to print on boot? ')
+            prompt='What message do you want to print on boot? ')
 @click.option('--font',
             default='slant',
             help='The font used in the kickass README.md ASCII banner (used with pyfiglet)')
 def main(name, output, font):
     """ Easily bootstrap an OS project to fool HR departments and pad your resume. """
 
-    directory_name = os.getcwd() + os.sep + name.lower().replace(' ', '-') +  os.sep
+    # The path of the directory where the final files will end up in
+    bootstrapped_directory = os.getcwd() + os.sep + name.lower().replace(' ', '-') +  os.sep
 
-    copy_tree(get_real_path(os.sep + 'my-cool-os-template'), directory_name)
+    # Copy the template files to the target directory
+    copy_tree(get_real_path(os.sep + 'my-cool-os-template'), bootstrapped_directory)
 
+    # Create the necessary assembly mov instructions for printing out the output on boot
     start_byte = int('0xb8000', 16)
     instructions_list = []
 
-    # Create the necessary assembly mov instructions for printing out the output on boot
     for c in output:
         char_as_hex = '0x02'+ c.encode('hex')
         instructions_list.append('\tmov word [{0}], {1} ; {2}'.format(hex(start_byte), char_as_hex, c))
         start_byte += 2
 
+    # Render the ASCII banner to be displayed in the README (A must for any serious hobby OS project!)
     banner = Figlet(font=font).renderText(name)
 
-    render_template_file(directory_name  + 'README.md', {'name' : name, 'banner' : banner})
-    render_template_file(directory_name  + 'grub.cfg' , {'name' : name})
-    render_template_file(directory_name  + 'boot.asm' , {'instructions_list' : instructions_list})
+    render_template_file(bootstrapped_directory  + 'README.md', {'name' : name, 'banner' : banner})
+    render_template_file(bootstrapped_directory  + 'grub.cfg' , {'name' : name})
+    render_template_file(bootstrapped_directory  + 'boot.asm' , {'instructions_list' : instructions_list})
 
-    print('finished bootstrapping OS project into directory ' + directory_name)
+    print('finished bootstrapping project into directory ' + bootstrapped_directory)
 
 if __name__ == '__main__':
     main()
